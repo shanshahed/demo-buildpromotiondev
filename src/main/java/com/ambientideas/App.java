f4eae2fcfb20b78d198fc958667c2aef02e1637c
msislamedevops <mail2msislam.devops@gmail.com>
2016-06-18 16:31:03 -0700

    first commit3

--- src/main/java/com/ambientideas/App.java
+++ src/main/java/com/ambientideas/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World shahed from CI again and again shahed sazzad Changed!" );
+        System.out.println( "Hello World shahed sazzad from CI again and again shahed sazzad Changed!" );
     }
 }
